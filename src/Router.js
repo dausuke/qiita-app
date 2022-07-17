837ffccc6d62d21a763afd2849e34e0341bf16ac
dausuke <ksye616@gmail.com>
2022-07-18 00:59:44 +0900

    vercelに変更

--- src/Router.js
+++ src/Router.js
@@ -1,15 +1,15 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import Home from './v1/pages/Home';
-import PostDetail from './v1/pages/PostDetail';
+import HomeV1 from './v1/pages/Home';
+import PostDetailV1 from './v1/pages/PostDetail';
 import App from './App';
 
 const Router = () => {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
       <Routes>
         <Route path='/' element={<App />}>
-          <Route path="v1" element={<Home />} />
-          <Route path="v1/post/:postId" element={<PostDetail />} />
+          <Route path="v1" element={<HomeV1 />} />
+          <Route path="v1/post/:postId" element={<PostDetailV1 />} />
         </Route>
       </Routes>
     </BrowserRouter>
